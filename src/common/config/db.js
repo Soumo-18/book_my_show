8f6610e27311a6fc89cc25b5605d3d340f571828
Soumo-18 <soumomandal7@gmail.com>
2026-04-14 17:46:57 +0530

    Update db config to use connectionString for production deployment

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,23 +1,25 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
+    
+    connectionString: process.env.DATABASE_URL || undefined,
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5433,
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'sql_class_2_db',
-    max:20,
-})
+    max: 20,
+});
 
-export const connectDB = async() =>{
-    try{
-        const client = await pool.connect()
-        console.log(`PostgreSql Connected: ${client.host}`)
-        client.release()
-    } catch(error){
-        console.error('Database Connection Failed',error)
-        process.exit(1)
+export const connectDB = async () => {
+    try {
+        const client = await pool.connect();
+        console.log(`PostgreSql Connected: ${client.host}`);
+        client.release();
+    } catch (error) {
+        console.error('Database Connection Failed', error);
+        process.exit(1);
     }
-}
+};
 
-export default pool
+export default pool;
